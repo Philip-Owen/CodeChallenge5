efe46f3f55ea648c067318bfae160b974274f086
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-01-29 13:32:00 -0600

    added front end elements and post request to post messages

--- server/public/scripts/controllers/messageBoard.controller.js
+++ server/public/scripts/controllers/messageBoard.controller.js
@@ -1,4 +1,17 @@
-messageApp.controller('MessageController', function() {
+messageApp.controller('MessageController', ['$http', function($http) {
     console.log('in MessageController');
     const self = this;
-})
+
+    self.newMessage = {};
+
+
+    self.postMessage = function(messageObject) {
+        $http.post('/messageBoard', messageObject)
+        .then(function (response) {
+            console.log('Message post: ', response);
+        })
+        .catch(function (response) {
+            console.log('Error with message post :', response);
+        })
+    }
+}])
